c92ddceb6e20a1d4d91e513f2c87fd4e4522ee74
lucas.singh@bizitglobal.com <lucas.sing@bizitglobal.com>
2025-08-14 13:55:17 -0300

    add vercel file4

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+    plugins: [
+        tailwindcss(),
+        react()
+    ],
+    base: '/reformas',
+    build: {
+        outDir: 'dist'
+    }
 })
